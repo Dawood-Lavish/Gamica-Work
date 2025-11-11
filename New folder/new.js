e7a407fa4a4065084f1733f27a060763ae081674
Dawood <144199678+Dawood-Lavish@users.noreply.github.com>
2025-11-11 22:34:41 +0500

    ok

---
+++ New folder/new.js
@@ -0,0 +1,26 @@
+// let str = "Fasilabad";
+// let ch ="";
+// let vowels = "";
+//         for(let i =0;i<=str.length-1 ;i++){
+//             ch = str[i];
+//              console.log(ch);
+//              if(ch=="a"||ch=="e"||ch=="i"||ch=="o"||ch=="u"){
+//                     vowels.includes(ch);
+//                 vowels += ch;
+//              }
+//              console.log(vowels);
+//         }
+
+
+
+    // let str = prompt("name")
+    // let vowels = '';
+    // for(let i = 0; i<str.length;i++){        
+    //     const ch =  str[i];        
+    //     if(ch == 'a' || ch == 'i' || ch == 'o' || ch == 'u'|| ch == 'e'){        
+    //         if(vowels.includes(ch) == false){
+    //             vowels += ch;
+    //         }
+    //     }
+    // }
+    //     console.log(vowels)
---
+++ New folder/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+let str = "Fasilabad";
+// let ch ="";
+// let vowels = "";
+//         for(let i =0;i<=str.length-1 ;i++){
+//             ch = str[i];
+//              console.log(ch);
+//              if(ch=="a"||ch=="e"||ch=="i"||ch=="o"||ch=="u"){
+//                 vowels +=ch;
+//              }
+//              console.log(vowels);
+//         }
