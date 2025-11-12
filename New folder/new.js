fc6491427dced9c55d989f8fac0d6f081d11f634
Dawood <144199678+Dawood-Lavish@users.noreply.github.com>
2025-11-12 15:09:29 +0500

    ok

--- New folder/new.js
+++ New folder/new.js
@@ -1,16 +1,3 @@
-// let str = "Fasilabad";
-// let ch ="";
-// let vowels = "";
-//         for(let i =0;i<=str.length-1 ;i++){
-//             ch = str[i];
-//              console.log(ch);
-//              if(ch=="a"||ch=="e"||ch=="i"||ch=="o"||ch=="u"){
-//                     vowels.includes(ch);
-//                 vowels += ch;
-//              }
-//              console.log(vowels);
-//         }
-
 
 
     // let str = prompt("name")
